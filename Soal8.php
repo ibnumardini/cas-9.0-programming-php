5fcfa4e9cad15ca060f8d26859fff791143afa7b
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-05-18 16:04:47 +0700

    feat(Soal8): solve

--- Soal8.php
+++ Soal8.php
@@ -2,5 +2,13 @@
 
 // input: 5
 
-$i = intval(5);
+$i = intval(readline());
 
+for ($a = 1; $a <= $i; $a++) {
+    for ($b = 1; $b <= $i; $b++) {
+        $c = $i - $a - $b;
+        if ($c > 0) {
+            echo sprintf("%d %d %d \n", $a, $b, $c);
+        }
+    }
+}
